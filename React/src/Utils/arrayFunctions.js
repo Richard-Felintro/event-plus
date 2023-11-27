e0b061ae5b78d98a4a1fda0a09920d69aa595af6
Richard Felintro <richardfelintro06@gmail.com>
2023-11-27 14:47:13 -0300

    Pastas reorganizadas.

--- React/src/Utils/arrayFunctions.js
+++
@@ -1,7 +0,0 @@
-export const arrayToAlphabeticalOrder = (data) => {
-  data.sort((a, b) => {
-    if (a.titulo < b.titulo) {
-      return -1;
-    }
-  });
-};
--- React/src/Utils/stringFunctions.js
+++
@@ -1,4 +0,0 @@
-export const dateFormatDbToView = data => {
-    data = data.substr(0,10).split("-")
-    return (`${data[2]}/${data[1]}/${data[0]}`)
-}
